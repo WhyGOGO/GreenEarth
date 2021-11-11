527b3723b1e3d8929c64c820f0cb31520307471a
nanowl <77216961+nanowl@users.noreply.github.com>
2021-11-11 11:09:44 +0900

    [한정엽] insert메소드 요구사항은 전부 구현완료 데이터 타입 수정필요

--- beans_method/insertDB.java
+++ beans_method/insertDB.java
@@ -24,7 +24,6 @@ public class insertDB {
 		pstmt.executeUpdate();
 	}
 
-	
 	// 회원가입한 정보를 DB에 추가
 	public void addCust(campDataBean member) throws Exception {
 		Connection conn = null;
@@ -44,8 +43,7 @@ public class insertDB {
 
 		pstmt.executeUpdate();
 	}
-	
-	
+
 	// 캠핑카 정보를 DB에 추가
 	public void addCar(campDataBean member) throws Exception {
 		Connection conn = null;
@@ -62,6 +60,72 @@ public class insertDB {
 		pstmt.setString(5, member.getRegistrationDate());
 		pstmt.setString(6, member.getNumberPeopleRide());
 		pstmt.setInt(7, member.getRentalCosts());
+
+		pstmt.executeUpdate();
+	}
+
+	// 정비소 정보를 DB에 추가
+	public void addRepair(campDataBean member) throws Exception {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		conn = DBUtil.getMySQLConnection(); // DB 연결
+		String sql = "insert into repairshop(repairShopId, carRepairNumber, repairShopName, repairShopAddress, repairShopCallNumber, repairShopNamepersonCharge, repairShopEmail) values(?,?,?,?,?,?,?)";
+		pstmt = conn.prepareStatement(sql);
+
+		pstmt.setInt(1, member.getRepairShopId());
+		pstmt.setInt(2, member.getCarRepairNumber());
+		pstmt.setString(3, member.getRepairShopName());
+		pstmt.setString(4, member.getRepairShopAddress());
+		pstmt.setInt(5, member.getRepairShopCallNumber());
+		pstmt.setString(6, member.getRepairShopNamepersonCharge());
+		pstmt.setString(7, member.getRepairShopEmail());
+
+		pstmt.executeUpdate();
+	}
+
+	// 캠핑카 대여정보를 DB에 추가
+	public void addRentInfo(campDataBean member) throws Exception {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		conn = DBUtil.getMySQLConnection(); // DB 연결
+		String sql = "insert into rental(rentalNumber, rentalCampingCarId, licenseNumber, rentalStartDate, rentalEndDate, rentalPeriod, requsetCharges, dueDatePayment, otherRequestDetails, otherClaims, rentalInformation) values(?,?,?,?,?,?,?,?,?,?,?)";
+		pstmt = conn.prepareStatement(sql);
+
+		pstmt.setInt(1, member.getRentalNumber());
+		pstmt.setInt(2, member.getRentalCampingCarId());
+		pstmt.setInt(3, member.getLicenseNumber());
+		pstmt.setString(4, member.getRentalStartDate());
+		pstmt.setString(5, member.getRentalEndDate());
+		pstmt.setString(6, member.getRentalPeriod());
+		pstmt.setString(7, member.getRequestCharges());
+		pstmt.setString(8, member.getDueDatePayment());
+		pstmt.setString(9, member.getOtherRequestDetails());
+		pstmt.setString(10, member.getOtherClaims());
+		pstmt.setString(11, member.getRentalInformation());
+
+		pstmt.executeUpdate();
+	}
+
+	// 정비의뢰 및 결과 정보를 추가
+	public void addRepairInfo(campDataBean member) throws Exception {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		conn = DBUtil.getMySQLConnection(); // DB 연결
+		String sql = "insert into maintain_info(carRepairNumber, campingCarId, repairShopId, licenseNumber, carRepairHistory, carRepairDate, carRepairCost, dueDatePayment, otherMaintenanceDetails) values(?,?,?,?,?,?,?,?,?)";
+		pstmt = conn.prepareStatement(sql);
+
+		pstmt.setInt(1, member.getCarRepairNumber());
+		pstmt.setInt(2, member.getCampingCarId());
+		pstmt.setInt(3, member.getRepairShopId());
+		pstmt.setInt(4, member.getLicenseNumber());
+		pstmt.setString(5, member.getCarRepairHistory());
+		pstmt.setString(6, member.getCarRepairDate());
+		pstmt.setInt(7, member.getCarRepairCost());
+		pstmt.setString(8, member.getDueDatePayment());
+		pstmt.setString(9, member.getOtherMaintenanceDetails());
 
 		pstmt.executeUpdate();
 	}
