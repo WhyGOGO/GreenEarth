d8e23d1c813225436b869e20370d46eb75c59902
WhyGOGO <minala7870@gmail.com>
2021-11-14 01:06:37 +0900

    [박영민] id getter setter 지움

--- beans/logonDataBean.java
+++ beans/logonDataBean.java
@@ -1,7 +1,6 @@
 package beans;
 
 public class logonDataBean {
-	private String id;
 	private String passwd;
 	private String name;
 	private String gender;
@@ -9,13 +8,6 @@ public class logonDataBean {
 	private String phone;
 	private String address;
 	private String licenseNumber;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
 	
 	public String getPasswd() {
 		return passwd;
